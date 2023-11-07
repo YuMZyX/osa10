7794a0a08dee1f5b893d46857964ad36ecc483df
YuMZyX <joni1koskinen@gmail.com>
2023-11-07 14:29:31 +0200

    Added exercise 10.2

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -4,18 +4,20 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! yeah!</Text>
       <Button title='test'>Test</Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+console.log('Is debugger working?')
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff087',
-    alignItems: 'top',
+    backgroundColor: '#fff000',
+    alignItems: 'center',
     justifyContent: 'center',
   },
 });
