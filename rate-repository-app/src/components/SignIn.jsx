49f4614c5821404c6841b7538379a4884f6b1b5a
YuMZyX <joni1koskinen@gmail.com>
2023-11-09 14:19:23 +0200

    Added 10.9 - 10.10

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,31 +1,39 @@
-import { View, StyleSheet, Pressable } from 'react-native';
-import FormikTextInput from './FormikTextInput';
-import { Formik } from 'formik';
-import theme from '../theme';
-import Text from './Text';
+import { View, StyleSheet, Pressable } from 'react-native'
+import FormikTextInput from './FormikTextInput'
+import { Formik } from 'formik'
+import * as yup from 'yup'
+import theme from '../theme'
+import Text from './Text'
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    padding: 15,
+    paddingLeft: 15,
+    paddingRight: 15,
     backgroundColor: theme.backgroundColors.bgWhite,
   },
   input: {
-    borderWidth: 1,
-    padding: 10,
-    borderColor: theme.colors.grey,
+    padding: 15,
     borderRadius: 10 / 2,
-    marginBottom: 15,
+    marginTop: 15,
     height: 55,
   },
   signIn: {
     backgroundColor: theme.backgroundColors.bgPrimary,
     borderRadius: 10 / 2,
-    padding: 10,
+    marginTop: 15,
+    marginBottom: 15,
     height: 55,
     textAlign: 'center',
     textAlignVertical: 'center',
   }
+})
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string().required('Username is required'),
+  password: yup
+    .string().required('Password is required'),
 })
 
 const SignIn = () => {
@@ -34,8 +42,9 @@ const SignIn = () => {
     console.log(values)
   }
 
+
   return (
-    <Formik initialValues={{ username: '', password: ''}} onSubmit={onSubmit}>
+    <Formik initialValues={{ username: '', password: ''}} onSubmit={onSubmit} validationSchema={validationSchema}>
       {({ handleSubmit }) => (
       <View style={styles.container}>
         <FormikTextInput style={styles.input} name='username' 
@@ -51,4 +60,4 @@ const SignIn = () => {
   )
 };
 
-export default SignIn;
+export default SignIn
